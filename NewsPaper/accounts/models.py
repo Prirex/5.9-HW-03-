d905da80c217f6b32cfb63ec656011b4613e7c8c
PriRex <prirex@yandex.ru>
2024-04-07 18:48:07 +0500

    first

---
+++ NewsPaper/accounts/models.py
@@ -0,0 +1,22 @@
+from django.db import models  # импорт
+
+
+class Order(models.Model):  # наследуемся от класса Model
+    pass
+
+
+class Product(models.Model):
+    pass
+
+
+class Staff(models.Model):
+    pass
+
+
+class ProductOrder(models.Model):
+    pass
+
+
+from django.db import models
+
+# Create your models here.
